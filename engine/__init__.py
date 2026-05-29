27e3bcad07d136b95419e09a330372b944ee3d6f
SsantiDev <ssantiago.xz1130@gmail.com>
2026-05-29 14:58:14 -0500

    chore(backend): clean up deprecated engine module initialization

--- engine/__init__.py
+++
@@ -1,9 +0,0 @@
-from .loader import cargar_davivienda, cargar_bancolombia, cargar_bogota, cargar_sap
-from .matcher import ejecutar_matching, ResultadoMatch, NivelConfianza, EstadoConciliacion
-from .diagnostics import diagnosticar, CategoriaDiagnostico
-
-__all__ = [
-    'cargar_davivienda', 'cargar_bancolombia', 'cargar_bogota', 'cargar_sap',
-    'ejecutar_matching', 'ResultadoMatch', 'NivelConfianza', 'EstadoConciliacion',
-    'diagnosticar', 'CategoriaDiagnostico',
-]
